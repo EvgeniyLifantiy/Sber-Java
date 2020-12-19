c7822da33d4adfb9a72bbdc2c090aaab0a1ca78c
Evgeniy <lifantii@inbox.ru>
2020-12-19 15:48:04 +0300

    add collection.task[1-3]

--- SecondTask/src/main/java/com/Presentation/Main.java
+++ SecondTask/src/main/java/com/Presentation/Main.java
@@ -1,6 +1,14 @@
 package com.Presentation;
 
+
 import com.Presentation.WordsCounter.Counter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static com.Presentation.WordsCounter.Counter.*;
 
 
 public class Main {
@@ -8,10 +16,25 @@ public class Main {
     public static void main(String[] args) {
 
         //Here you can input your test text
-        String text="Преобразует все буквы в строке к нижнему регистру. Метод toLowerCase не применяется к знакам, отличным от букв. В следующем примере демонстрируется действие метода toLowerCase.";
-        Counter counter =new Counter();
-        counter.Count(text);
-        System.out.println(counter.getWordcounter());
+        String text="Преобразует все буквы в строке к нижнему регистру. Метод не применяется к знакам, отличным от букв. В следующем примере демонстрируется действие метода toLowerCase.";
+
+        Count(text);// count every word in text
+                    //so ,we must count words in text and then can show unique words or amount of every word
+
+        //Task 1
+        CountUniqueWords(text);
+        System.out.println("Task 1 : Unique words in text "+getUniqueWords());
+
+        //Task 2
+        String[] strings= splitstring(text);
+        Collections.sort(getUniqueSortedList(),new Counter());
+        System.out.println("Task 2 :"+getUniqueSortedList());
+
+        //Task 3
+        System.out.println("Task 3 : Count and display the number of times each word occurs in the file.");
+        System.out.println(getWordcounter());//  get how many times this word appeared in the text
+
+
     }
 
 
--- SecondTask/src/main/java/com/Presentation/WordsCounter/Counter.java
+++ SecondTask/src/main/java/com/Presentation/WordsCounter/Counter.java
@@ -1,33 +1,73 @@
 package com.Presentation.WordsCounter;
 
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 
-public class Counter {
+public class Counter implements Comparator<String> {
 
-    private HashMap<String,Integer> wordcounter= new HashMap<>();
+    private static HashMap<String,Integer> wordcounter= new HashMap<>();
 
-    public HashMap<String, Integer> getWordcounter() {
+    public static ArrayList<String> getUniqueSortedList() {
+        return UniqueSortedList;
+    }
+
+    private  static ArrayList<String> UniqueSortedList=new ArrayList();
+
+    private static int UniqueWords;
+
+    public static HashMap<String, Integer> getWordcounter() {
         return wordcounter;
     }
 
-    public void setWordcounter(HashMap<String, Integer> wordcounter) {
-        this.wordcounter = wordcounter;
+    public static String[] splitstring(String str){
+        str=str.replaceAll("[!-?]+", ""); //delete all punctuation mark
+
+        String[] str1=str.split(" "); //convert to string array
+        return str1;
     }
 
-    public  void Count(String str){
-        str=str.replaceAll("[!-?]+", ""); //delete all punctuation mark
-        String[] str1=str.split(" ");
+    public static void Count(String str){ //this method count , how many every word meet in text
+
+        wordcounter.clear();
+
+        String[] str1=splitstring(str);
 
        for (int i=0;i< str1.length;i++){
-           if (wordcounter.containsKey(str1[i].toLowerCase())){ // if this word exist, then increment his amount
-               wordcounter.replace(str1[i].toLowerCase(),wordcounter.get(str1[i].toLowerCase())+1);
-           }  else { //else create new key
-               wordcounter.put(str1[i].toLowerCase(),1);
+           if (wordcounter.containsKey(str1[i].toLowerCase())){ // if this word exist
+               wordcounter.replace(str1[i].toLowerCase(),wordcounter.get(str1[i].toLowerCase())+1);//then increment his amount
+           }  else {
+               wordcounter.put(str1[i].toLowerCase(),1);//else create new key
 
            }
        }
     }
 
+    public static void CountUniqueWords(String str){ // when we count all words we can determine how many uniaue words
+        UniqueWords=0;
+        for (String key: wordcounter.keySet()){
+            if (wordcounter.get(key)==1){
+                UniqueWords++;
+                UniqueSortedList.add(key);
+            }
+        }
+    }
+
+    public static int getUniqueWords(){
+        return UniqueWords;
+    }
+
+
+    @Override
+    public int compare(String str1,String str2) { // here we override the comparison method to compare strings in ascending order
+        if (str1.length() > str2.length()) {
+            return 1;
+        } else if (str1.length()<str2.length()) {
+            return -1;
+        } else {
+            return str1.compareTo(str2);
+        }
+    }
 
 }
