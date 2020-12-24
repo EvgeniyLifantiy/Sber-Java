b11f554eb589753a51299f93bd315192f7da5a75
Evgeniy <lifantii@inbox.ru>
2020-12-24 21:33:30 +0300

    Add CollectionUtils

--- ThirdTask/src/main/java/com/PECS/CollectionUtils.java
+++ ThirdTask/src/main/java/com/PECS/CollectionUtils.java
@@ -1,28 +1,75 @@
 package com.PECS;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class CollectionUtils {
+
+
+
     public static<T> void addAll(List<? extends T> source, List<? super T> destination) {
         destination.addAll(source);
     }
 
-    public static List newArrayList() { }
+    public static<T> List<T> newArrayList() {
+        return (new ArrayList<T>());
+    }
 
-    public static int indexOf(List source, Object o) { }
+    public static <T> int indexOf(List<T> source, T elem) {
+        return source.indexOf(elem);
+    }
 
-    public static List limit(List source, int size) { }
+    public static <T> List<T> limit(List<? extends T> source, int size) {
+        List<T> destination=new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            destination.add(source.get(i));
+        }
 
-    public static void add(List source, Object o) { }
+        return destination;
+    }
 
-    public static void removeAll(List removeFrom, List c2) { }
+    public static <T>void add(List<? super T> source, T elem) {
+        source.add(elem);
+    }
 
-    public static boolean containsAll(List c1, List c2) { }
+    public static <T>void removeAll(List<? extends T> removeFrom, List<? super T> c2) {
+        removeFrom.removeAll(c2);
+    }
 
-    public static boolean containsAny(List c1, List c2) { }
+    public static <T> boolean containsAll(List<? extends T> c1, List<? extends T> c2) {
+        if (c1.containsAll(c2)){
+            return true;
+        }
+        return false;
+    }
 
-    public static List range(List list, Object min, Object max) { }
+    public static <T> boolean containsAny(List<? extends T> c1, List<? extends T> c2) {
+        for (T elem:c2) {
+           if(c1.contains(elem)){
+               return true;
+           }
+        }
+        return false;
 
-    public static List range(List list, Object min, Object max, Comparator comparator) { }
+    }
+
+    public static<T extends Comparable<? super T>> List<T> range(List<? extends T> list, T min, T max) {
+
+        List<T> rangelist=new ArrayList<>();
+        for (T elem :list){
+            if (elem.compareTo(min)>=0 && elem.compareTo(max)<=0){
+                rangelist.add(elem);
+            }
+
+        }
+
+        return rangelist;
+
+    }
+
+//    public static List range(List list, Object min, Object max, Comparator comparator) {
+//
+//    }
 }
 
--- ThirdTask/src/main/java/com/PECS/Main.java
+++ ThirdTask/src/main/java/com/PECS/Main.java
@@ -1,7 +1,69 @@
 package com.PECS;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.PECS.CollectionUtils.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        //
+
+
+        //test newArrayList() and test addAll()
+        System.out.println("-----------------------------------------------------");
+        List <Integer> list=newArrayList();
+        System.out.println("Сейчас лист пуст "+list);
+        List<Integer> sourcelist=new ArrayList<>();
+        sourcelist.add(12);
+        sourcelist.add(76);
+        sourcelist.add(346);
+        sourcelist.add(54);
+        sourcelist.add(2);
+        addAll(sourcelist,list);
+        System.out.println("А теперь в него перенесены все элементы "+list);
+
+        //test limit()
+        System.out.println("-----------------------------------------------------");
+        List<Integer> shortlist=limit(list,3);
+        System.out.println("Укороченный лист выглядит так: "+shortlist);
+
+        // test add()
+        System.out.println("-----------------------------------------------------");
+        add(list,231);
+        System.out.println("Теперь лист выглядит вот так: "+list);
+
+        //test indexOf()
+        System.out.println("-----------------------------------------------------");
+        System.out.println("индекс элемента 76 - "+indexOf(list,76));
+
+
+        //test removeAll()
+        System.out.println("-----------------------------------------------------");
+        removeAll(list,shortlist);
+        System.out.println("После вычитания лист стал таким : "+list);
+
+
+
+        //test containAll
+        System.out.println("-----------------------------------------------------");
+        System.out.println("То ,что sourcelist содержит все элементы shortlist - "+containsAll(sourcelist,shortlist));
+
+
+        //test containAny
+        System.out.println("-----------------------------------------------------");
+        System.out.println("То,что sourcelist содержит хотя бы один элементе list - "+containsAny(sourcelist,list));
+
+        //test range
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Все элементы sourcelist которые больше 22 и меньше 344"+range(sourcelist,22,344));
+
+
+        //test range with comparable
+        System.out.println("-----------------------------------------------------");
+
+
 
     }
 }
